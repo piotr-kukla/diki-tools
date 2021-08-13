38131c1a05ac91c6cc3d426f1e141d3be9c6e1b1
Piotr Kukla <piotr.kukla@picadilla.com>
2021-08-13 13:08:16 +0200

    different pring

--- src/main/scala/com/kuki/dikitools/model/Translation.scala
+++ src/main/scala/com/kuki/dikitools/model/Translation.scala
@@ -6,6 +6,7 @@ import zio.console._
 
 case class Translation(word: Word, sentence: String, translation: String) {
 
-  def print() = putStrLn(s"$sentence $translation")
+  //def print() = putStrLn(s"$sentence $translation")
+  def print() = putStr(translation) *> getStrLn *> putStrLn(sentence)
   def toCsv() = s"$word|$sentence|$translation|0"
 }
