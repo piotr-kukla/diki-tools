3bd44282b317e2da9ebd29da54672c879dea0003
Piotr Kukla <piotr.kukla@picadilla.com>
2021-08-23 09:34:21 +0200

    known words memo layer

--- src/main/scala/com/kuki/dikitools/CheckWord.scala
+++ src/main/scala/com/kuki/dikitools/CheckWord.scala
@@ -1,6 +1,5 @@
 package com.kuki.dikitools
 
-import com.kuki.dikitools.service.KnownWordsService
 import com.kuki.dikitools.service._
 import zio._
 import zio.blocking.Blocking
@@ -12,16 +11,16 @@ object CheckWord extends App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
 
 
-    val program: ZIO[Blocking with Console with WordChecker, Throwable, Unit] = for {
+    val program: ZIO[Blocking with Console with WordChecker with KnownWords, Throwable, Unit] = for {
       _    <- putStrLn("Check world: ")
       word <- getStrLn
       translations <- checkWord(word)
       _    <- ZIO.foreach(translations)(_.print())
-      _    <- KnownWordsService.updateKnownWords(word, translations)
+      _    <- updateKnownWords(word, translations)
     } yield ()
 
     program
-      .provideCustomLayer(WordChecker.live)
+      .provideCustomLayer(WordChecker.live ++ KnownWordsMemo.live)
       .repeat(Schedule.forever)
       .exitCode
   }
---
+++ src/main/scala/com/kuki/dikitools/service/KnownWordsMemo.scala
@@ -0,0 +1,54 @@
+package com.kuki.dikitools.service
+
+import java.io.{BufferedWriter, FileWriter}
+
+import com.kuki.dikitools.Word
+import com.kuki.dikitools.model.Translation
+import zio.{Has, ULayer, ZIO, ZLayer}
+import zio.blocking.{Blocking, effectBlocking}
+
+import scala.io.{Codec, Source}
+
+object KnownWordsMemo extends Serializable {
+
+  trait Service {
+    def updateKnownWords(newWord: Word, translations: List[Translation]): ZIO[Blocking, Throwable, Unit]
+  }
+
+  object Service {
+
+    val WORDS_FILE_NAME = "words"
+    val SENTENCES_FILE_NAME = "sentences"
+
+    val live: Service = (newWord: Word, translations: List[Translation]) =>
+      for {
+        knownWords <- loadWords()
+        _ <- if (knownWords.contains(newWord)) ZIO.succeed() else updateDatabase(newWord, translations)
+      } yield ()
+
+    private def loadWords(): ZIO[Blocking, Throwable, Set[Word]] = effectBlocking {
+      Source.fromResource(WORDS_FILE_NAME)(Codec.UTF8).getLines.toSet
+    }
+
+    private def updateDatabase(word: Word, translations: List[Translation]): ZIO[Blocking, Throwable, Unit] =
+      for {
+        _ <- appendNewWord(word)
+        _ <- appendNewSentences(translations)
+      } yield ()
+
+    private def appendNewWord(word: Word): ZIO[Blocking, Throwable, Unit] = effectBlocking {
+      val writer = new BufferedWriter(new FileWriter(s"src/main/resources/$WORDS_FILE_NAME", true))
+      writer.write(s"$word\n")
+      writer.close()
+    }
+
+    private def appendNewSentences(translations: List[Translation]) = effectBlocking {
+      val writer = new BufferedWriter(new FileWriter(s"src/main/resources/$SENTENCES_FILE_NAME", true))
+      writer.write(translations.map(_.toCsv()).mkString("\n"))
+      writer.close()
+    }
+  }
+
+  val live: ULayer[Has[Service]] = ZLayer.succeed(Service.live)
+
+}
