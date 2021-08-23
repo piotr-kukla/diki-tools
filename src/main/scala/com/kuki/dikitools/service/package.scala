027a4cdc969a295a6c8bacacc2dcdf988f43d16b
Piotr Kukla <piotr.kukla@picadilla.com>
2021-08-23 09:34:29 +0200

    known words memo layer

--- src/main/scala/com/kuki/dikitools/service/package.scala
+++ src/main/scala/com/kuki/dikitools/service/package.scala
@@ -7,8 +7,12 @@ import zio.blocking.Blocking
 package object service {
 
   type WordChecker = Has[WordChecker.Service]
+  type KnownWords = Has[KnownWordsMemo.Service]
 
   def checkWord(word: Word): ZIO[Blocking with WordChecker, Throwable, List[Translation]] =
     ZIO.accessM(_.get[WordChecker.Service].checkWord(word))
+
+  def updateKnownWords(newWord: Word, translations: List[Translation]): ZIO[Blocking with KnownWords, Throwable, Unit] =
+    ZIO.accessM(_.get[KnownWordsMemo.Service].updateKnownWords(newWord, translations))
 
 }
